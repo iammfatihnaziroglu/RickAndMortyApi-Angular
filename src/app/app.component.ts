a17cce08a640957c0a5bfd8b20ff72fbc6c4d901
M.Fatih <m.fatihnaziroglu@gmail.com>
2025-01-28 01:23:03 +0300

    rickandmortyapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RickAndMortyApi-Angular';
+  characters: Character[] = [];
+  currentPage = 1;
+  totalPages = 1;
+
+  constructor(private http: HttpClient) {
+    this.loadCharacters();
+  }
+
+  loadCharacters(page: number = 1) {
+    const apiUrl = `https://rickandmortyapi.com/api/character?page=${page}`;
+    
+    this.http.get<any>(apiUrl).subscribe({
+      next: (response) => {
+        this.characters = response.results;
+        this.totalPages = response.info.pages;
+        this.currentPage = page;
+      },
+      error: (err) => console.error('API Hatası:', err)
+    });
+  }
+
+  changePage(newPage: number) {
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      this.loadCharacters(newPage);
+    }
+  }
 }
